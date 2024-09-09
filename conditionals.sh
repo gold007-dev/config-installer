dbd606f10c00a3f42c2e19cf9b3bb8b9d8c8ef57
David Hafner <david.hafner@tingo.ch>
2024-09-09 15:07:36 +0200

    copied conditional checks from arch

---
+++ conditionals.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "checking if you are in a wsl"
+if [[ $(fastfetch --host-format {1} | grep -e "Host: WSL") ]]; then
+    if [[ ! $(grep -e '# Check if we are currently in the home of the host' ~/.bashrc) ]]; then
+        echo ""
+        echo '# Check if we are currently in the home of the host' >>~/.bashrc
+        echo 'regex="/mnt/c/Users/[^/]*/?$";' >>~/.bashrc
+        echo '' >>~/.bashrc
+        echo 'if [[ $(pwd) =~ $regex ]];then' >>~/.bashrc
+        echo 'echo "sui"' >>~/.bashrc
+        echo 'fi' >>~/.bashrc
+    fi
+fi
