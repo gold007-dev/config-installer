95a02d0817b0a97e0b33edb28b46a0f1ce8769d2
David Hafner <david.hafner@tingo.ch>
2024-09-09 15:12:28 +0200

    added conditionals to install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+echo "updating"
 sudo pacman -Syi
+echo "installing packages"
 sudo pacman -S zsh -y
 sudo pacman -S fastfetch -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -8,6 +9,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # /usr/local/share/fonts
+echo "installing fonts"
 sudo mkdir -p /usr/local/share/fonts/
 
 sudo curl "https://raw.githubusercontent.com/romkatv/dotfiles-public/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf" -o "/usr/local/share/fonts/MesloLGS NF Bold Italic.ttf"
@@ -15,6 +17,10 @@ sudo curl "https://raw.githubusercontent.com/romkatv/dotfiles-public/master/.loc
 sudo curl "https://raw.githubusercontent.com/romkatv/dotfiles-public/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Italic.ttf" -o "/usr/local/share/fonts/MesloLGS NF Italic.ttf"
 sudo curl "https://raw.githubusercontent.com/romkatv/dotfiles-public/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Regular.ttf" -o "/usr/local/share/fonts/MesloLGS NF Regular.ttf"
 
+echo "copying config files"
 cp .bashrc ~/.bashrc
 cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
+
+echo "running conditional checks"
+bash conditionals.sh
