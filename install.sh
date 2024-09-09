ad04d284c6932f7d9ded4893a76fa4f49c1fb561
David Hafner <david.hafner@tingo.ch>
2024-09-09 15:12:55 +0200

    added fastfetch to installs

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt update
 
 echo "installing packages"
 sudo apt install zsh -y
+sudo apt install fastfetch -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
