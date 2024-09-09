42f3f34e9426b5cc24775c5abbccd6513aacc442
David Hafner <david.hafner@tingo.ch>
2024-09-09 15:06:08 +0200

    added conditional checks

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 sudo pacman -Syi
 sudo pacman -S zsh -y
+sudo pacman -S fastfetch -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
--- update-config.sh
+++ update-config.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-read -p "do you want to pull the latest version?" update
+echo "Do you want to pull the latest version?"
+read update
 
 regex='^y(es)?$'
 update="$(echo "$update" | tr '[:upper:]' '[:lower:]')"
 
 if [[ $update =~ $regex ]]; then
-  git pull
+    git pull
 fi
+
+echo "copying config files"
 
 cp .bashrc ~/.bashrc
 cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
+
+echo "running conditional checks"
+bash conditionals.sh
