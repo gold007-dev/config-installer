873f073230a7ee6976d2d6dc88554cff62dab28f
gold007-dev <gold007133@gmail.com>
2024-09-10 11:26:58 +0200

    added line break before conditionals

--- conditionals.sh
+++ conditionals.sh
@@ -3,7 +3,7 @@
 echo "checking if you are in a wsl"
 if [[ $(fastfetch --host-format {1} | grep -e "Host: WSL") ]]; then
     if [[ ! $(grep -e '# Check if we are currently in the home of the host' ~/.bashrc) ]]; then
-        echo ""
+        echo "" >> ~/.bashrc
         echo '# Check if we are currently in the home of the host' >>~/.bashrc
         echo 'regex="/mnt/c/Users/[^/]*/?$";' >>~/.bashrc
         echo '' >>~/.bashrc
