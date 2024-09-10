8f3db97ed2d558f9724dd742f942a924fcdf96ec
gold007-dev <gold007133@gmail.com$>
2024-09-10 19:25:51 +0200

    fixed fastfetch install

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt update
 echo "installing packages"
 sudo apt install zsh -y
 sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
-sudo apt update
+sudo apt install fastfetch
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
